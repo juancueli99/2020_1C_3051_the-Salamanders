31e9dd6970c2d4fc151afa2081c0363af26a150f
jjf-covello <covellojorge@gmail.com>
2020-06-07 15:38:08 -0300

    agrego sonido como clase falta implementarlo en GameModel

---
+++ TGC.Group/Model/Sonido.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGC.Core.Direct3D;
+using TGC.Core.Sound;
+using TGC.Core.Direct3D;
+
+namespace TGC.Group.Model
+{
+    class Sonido
+    {
+        TgcStaticSound player = new TgcStaticSound();
+        String MediaDir = "..\\..\\..\\Media\\";
+        String ReproduccionActual;
+        private static TgcDirectSound DirectSound = new TgcDirectSound();
+        public Sonido(String archivo, bool loop) {
+
+            ReproduccionActual = archivo;
+            var device= DirectSound.DsDevice;
+            player.loadSound(MediaDir + archivo,device);
+            player.play(loop);
+            
+        }
+        public void DetenerSonido() {
+            player.stop();
+        }
+
+        public void CambiarSonido(String archivo, bool loop) {
+            this.DetenerSonido();
+            this.CargarSonido(archivo);
+            this.escucharSonidoActual(loop);
+        
+        }
+
+        private void escucharSonidoActual(bool loop)
+        {
+            player.play(loop);
+        }
+
+        private void CargarSonido(string archivo)
+        {
+            var device = DirectSound.DsDevice;
+            player.loadSound(MediaDir + archivo, device);
+        }
+    }
+}
