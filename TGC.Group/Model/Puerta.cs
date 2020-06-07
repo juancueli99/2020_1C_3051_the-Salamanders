c600d18f97d8ab38b6ee8ecd595a48a22d2b6011
jjf-covello <covellojorge@gmail.com>
2020-06-07 17:28:29 -0300

    agrego sonido de puerta

--- TGC.Group/Model/Puerta.cs
+++ TGC.Group/Model/Puerta.cs
@@ -16,11 +16,14 @@ namespace TGC.Group.Model
         public Puerta otroExtremo;// por si quieren cambiar ese if asqueroso
         TGCVector3 posicionEntrada = new TGCVector3(-1222, 15, -6857);
         TGCVector3 posicionSalida = new TGCVector3(-1200, 15, -7500);
+        Sonido sonidoApertura;
+        Sonido sonidoCierre;
 
 
         public Puerta(TgcMesh mesh)
         {
-
+            sonidoApertura = new Sonido("puerta ruidosa, abrir.wav", -3900,false);
+            sonidoCierre = sonidoApertura; //as tincho asked
             meshAsociado = mesh;
         }
 
@@ -32,6 +35,7 @@ namespace TGC.Group.Model
         public void Interactuar(Personaje personaje)
         {
             this.Usar(personaje);
+
         }
 
         public void Usar(Personaje personaje)
@@ -40,11 +44,13 @@ namespace TGC.Group.Model
             {
                 personaje.TeletrasportarmeA(posicionSalida);
                 personaje.setearSonidosOutdoor();
+                sonidoApertura.escucharSonidoActual(false);
             }
             else
             {
                 personaje.TeletrasportarmeA(posicionEntrada);
                 personaje.setearSonidosIndoor();
+                sonidoCierre.escucharSonidoActual(false);
             }
 
             personaje.estoyAdentro = !personaje.estoyAdentro;
